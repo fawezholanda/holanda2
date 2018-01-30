48253487112a3e6fa713185136c99dec22c120ea
fawezholanda <fawezholanda@hotmail.com>
2018-01-30 11:32:08 +0000

    Lesson 12 finished

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
   end
 
   def index
-    @places = Place.order("name").page(params[:page])
+    @places = Place.order("name").page(params[:page]).per_page(10)
   end
 
 end
