867b557339aa5bb36c4f0f313b99f878af044480
fawezholanda <fawezholanda@hotmail.com>
2018-02-02 11:38:47 +0000

    Lesson 19 in progress

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,10 +2,6 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.all 
-  end
-
-  def index
     @places = Place.order("name").page(params[:page]).per_page(10)
   end
 
